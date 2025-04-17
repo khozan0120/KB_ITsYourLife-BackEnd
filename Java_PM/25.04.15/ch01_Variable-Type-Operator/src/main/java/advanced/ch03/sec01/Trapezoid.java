9668aeb7d79f90a17210636fd36eb55b3124f4d5
DESKTOP-RS7BK7V\A <fumajin0120@gmail.com>
2025-04-18 02:50:43 +0900

    Operator심화

---
+++ Java_PM/25.04.15/ch01_Variable-Type-Operator/src/main/java/advanced/ch03/sec01/Trapezoid.java
@@ -0,0 +1,12 @@
+package advanced.ch03.sec01;
+
+public class Trapezoid {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 10;
+        int z = 7;
+
+        double result = ((x + y) * z) /2;
+        System.out.println("result : " + result);
+    }
+}
