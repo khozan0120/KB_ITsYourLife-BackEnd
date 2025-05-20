96a3065fc2cb66765b796b8ddeb7e8b33e5f966f
khozan0120 <fumajin0120@gmail.com>
2025-05-20 16:51:05 +0900

    advanced_문제풀이 수정

--- MySQL/03_DataBase/25.05.20/06_index/advanced/01_sqldb/01_usertbl.sql
+++ MySQL/03_DataBase/25.05.20/06_index/advanced/01_sqldb/01_usertbl.sql
@@ -5,6 +5,8 @@ select * from usertbl;
 -- usertbl의 인덱스 목록 확인
 show index from usertbl;
 -- usertbl의 데이터 크기와 인덱스 크기 확인
+analyze table usertbl;
+show table status  like "usertbl";
 
 -- usertbl의addr 컬럼에 대해idx_usertbl_addr이름으로 인덱스를 만들고,
 -- 인덱스목록을확인하세요.
@@ -16,6 +18,8 @@ show index  from usertbl;
 -- 내용이없다면실제적용되도록한후,
 create index idx_usertbl on usertbl(userID);
 -- 인덱스의크기를확인하세요.
+analyze table usertbl;
+show table status like 'usertbl';
 
 -- usertbl에대해다음을처리하세요.
 -- birthYear 컬럼에 대해 idx_usertbl_birthYear 이름의 인덱스를 만드세요.
